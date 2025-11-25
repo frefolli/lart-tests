3f0a2643214cbb95cd7650a0c3bcc1e005edd736
Francesco Refolli <f.refolli@campus.unimib.it>
2025-11-25 23:44:47 +0100

    another success for the Lart Communist Party

--- main.py
+++ main.py
@@ -31,15 +31,22 @@ class CMD:
   def assemble(self) -> str:
     return ' '.join([self.cmd] + self.args)
 
+  def exec(self) -> int:
+    cmdline = self.assemble()
+    print('|>', cmdline)
+    return os.system(cmdline)
+
 class Compiler:
-  def __init__(self, path: str, include_directories: list[str]) -> None:
+  def __init__(self, path: str, include_directories: list[str], options: list[str]) -> None:
     self.path: str = path
     self.include_directories: list[str] = include_directories
+    self.options: list[str] = options
 
   def to_dict(self) -> dict:
     return {
       'path': self.path,
-      'include_directories': self.include_directories
+      'include_directories': self.include_directories,
+      'options': self.options,
     }
 
   @staticmethod
@@ -47,6 +54,7 @@ class Compiler:
     return Compiler(
       path = data['path'],
       include_directories = data['include_directories'],
+      options = data['options'],
     )
 
   def compile(self, sources: list[str], links: list[str], output: str) -> int:
@@ -54,11 +62,12 @@ class Compiler:
     cmd.append(['-I' + include_directory for include_directory in self.include_directories])
     cmd.append(['-l' + link for link in links])
     cmd.append(sources)
+    cmd.append(self.options)
     cmd.append(['-o', output])
+    if output.endswith('.o'):
+      cmd.append('-c')
 
-    print('|>', cmd.assemble())
-    # return os.system(cmd.assemble())
-    return 0
+    return cmd.exec() == 0
 
 class TestKind(enum.Enum):
   SUCC='succ'
@@ -132,33 +141,80 @@ class Test:
     for c_source in c_sources:
       o_source = c_source.replace('.c', '.o')
       if o_source in o_sources:
-        raise ValueError('Conflicting CC::out vs LartC::in => `%s`' % (o_source,))
+        raise ValueError('Conflicting CC::out vs LARTC::in => `%s`' % (o_source,))
       o_sources.add(o_source)
-      framework.cc.compile([c_source], [], o_source)
+      ok = framework.cc.compile([c_source], [], o_source)
+      if not ok:
+        return
+
+    for lart_source in lart_sources:
+      o_source = lart_source.replace('.lart', '.o')
+      if o_source in o_sources:
+        raise ValueError('Conflicting LARTC::out vs LARTC::in => `%s`' % (o_source,))
+      o_sources.add(o_source)
+      ok = framework.lartc.compile([lart_source], [], o_source)
+      if not ok:
+        return
+
+    for ll_source in ll_sources:
+      o_source = ll_source.replace('.ll', '.o')
+      if o_source in o_sources:
+        raise ValueError('Conflicting LLVMIR::out vs LARTC::in => `%s`' % (o_source,))
+      o_sources.add(o_source)
+      ok = framework.lartc.compile([ll_source], [], o_source)
+      if not ok:
+        return
+
+    for s_source in s_sources:
+      o_source = s_source.replace('.s', '.o')
+      if o_source in o_sources:
+        raise ValueError('Conflicting AS::out vs LARTC::in => `%s`' % (o_source,))
+      o_sources.add(o_source)
+      ok = framework.lartc.compile([s_source], [], o_source)
+      if not ok:
+        return
 
     o_sources = list(o_sources)
-    sources = lart_sources + ll_sources + s_sources + o_sources
     program = os.path.join(self.path, self.program)
-    framework.lartc.compile(sources, self.links, program)
+    ok = framework.lartc.compile(o_sources, self.links, program)
+    if not ok:
+      return
 
   def run(self, framework: Framework) -> None:
-    pass
+    program = os.path.join(self.path, self.program)
+    cmd = CMD(program)
+    for input in self.inputs:
+      cmd.append(['<', os.path.join(self.path, input)])
+    cmd.append(['>', os.path.join(self.path, self.output)])
+    cmd.exec()
 
   def clean(self, framework: Framework) -> None:
     sources = [os.path.join(self.path, source) for source in self.sources]
     c_sources = list(filter(lambda f: f.endswith('.c'), sources))
+    lart_sources = list(filter(lambda f: f.endswith('.lart'), sources))
+    ll_sources = list(filter(lambda f: f.endswith('.ll'), sources))
+    s_sources = list(filter(lambda f: f.endswith('.s'), sources))
     
-    o_source = [c_source.replace('.c', '.o') for c_source in c_sources]
+    o_sources = []
+    o_sources += [c_source.replace('.c', '.o') for c_source in c_sources]
+    o_sources += [lart_source.replace('.lart', '.o') for lart_source in lart_sources]
+    o_sources += [ll_source.replace('.ll', '.o') for ll_source in ll_sources]
+    o_sources += [s_source.replace('.s', '.o') for s_source in s_sources]
     program = os.path.join(self.path, self.program)
     output = os.path.join(self.path, self.output)
 
     cmd = CMD('rm')
     cmd.append('-rf')
-    cmd.append(o_source)
+    cmd.append(o_sources)
     cmd.append(program)
     cmd.append(output)
-    print('|>', cmd.assemble())
-    # os.system(cmd.assemble())
+    cmd.exec()
+
+  def report(self, framework: Framework) -> None:
+    print('#' * 20 + self.name.ljust(20) + '#' * 20)
+    with open(os.path.join(self.path, self.output)) as file:
+      print(file.read())
+    print('#' * 20 + self.name.ljust(20) + '#' * 20)
 
   def to_dict(self) -> dict:
     return {
@@ -213,8 +269,8 @@ class Framework:
       return Framework.from_dict(read_yaml(config_file))
     else:
       framework = Framework(
-        cc = Compiler('./cc', []),
-        lartc = Compiler('./lartc', ['include']),
+        cc = Compiler('./cc', [], ['-c']),
+        lartc = Compiler('./lartc', ['include'], []),
         test_dir = 'tests'
       )
       write_yaml(config_file, framework.to_dict())
@@ -250,47 +306,98 @@ class Framework:
     else:
       return {TestKind.parse(kind):[Test.from_dict(test) for test in tests] for kind, tests in read_yaml(test_config).items()}
 
-  def clean(self):
-    clean_list: list[Test] = []
-    clean_list += (self.tests.get(TestKind.SUCC) or [])
-    clean_list += (self.tests.get(TestKind.DIFF) or [])
+  def get_targets(self, raw_targets: list[str]) -> list[Test]:
+    targets: list[Test] = []
+    kinds: dict[TestKind, set[str]] = {}
+    for raw_target in raw_targets:
+      pieces = raw_target.split('/')
+      if len(pieces) != 2:
+        raise ValueError('Invalid target supplied: `%s` is not in format <kind>/<name>' % (raw_target,))
+      kind, name = pieces
+      if kind not in TestKind.values():
+        raise ValueError('Invalid test kind in target supplied: `%s` is not a valid TestKind' % (kind,))
+      kind = TestKind.parse(kind)
+      if kind not in kinds:
+        kinds[kind] = set({})
+      kinds[kind].add(name)
 
-    for test in clean_list:
+    for kind in kinds.keys():
+      names = kinds[kind]
+      tests_of_that_kind: list[Test] = (self.tests.get(kind) or [])
+      filtered_tests = list(filter(lambda t: t.name in names, tests_of_that_kind))
+      filtered_names = set([t.name for t in filtered_tests])
+      missing_names = names.difference(filtered_names)
+      if len(missing_names) > 0:
+        raise ValueError('Invalid test names supplied: %s are not valid test names within kind `%s`' % (names, kind))
+      targets += filtered_tests
+    return targets
+
+  def clean(self, raw_targets: list[str]):
+    targets: list[Test] = []
+    if len(raw_targets) == 0:
+      targets += (self.tests.get(TestKind.SUCC) or [])
+      targets += (self.tests.get(TestKind.DIFF) or [])
+    else:
+      targets = self.get_targets(raw_targets)
+
+    for test in targets:
       test.clean(self)
 
-  def build(self):
-    build_list: list[Test] = []
-    build_list += (self.tests.get(TestKind.SUCC) or [])
-    build_list += (self.tests.get(TestKind.DIFF) or [])
+  def build(self, raw_targets: list[str]):
+    targets: list[Test] = []
+    if len(raw_targets) == 0:
+      targets += (self.tests.get(TestKind.SUCC) or [])
+      targets += (self.tests.get(TestKind.DIFF) or [])
+    else:
+      targets = self.get_targets(raw_targets)
 
-    for test in build_list:
+    for test in targets:
       test.build(self)
 
-  def run(self):
-    run_list: list[Test] = []
-    run_list += (self.tests.get(TestKind.SUCC) or [])
-    run_list += (self.tests.get(TestKind.DIFF) or [])
+  def run(self, raw_targets: list[str]):
+    targets: list[Test] = []
+    if len(raw_targets) == 0:
+      targets += (self.tests.get(TestKind.SUCC) or [])
+      targets += (self.tests.get(TestKind.DIFF) or [])
+    else:
+      targets = self.get_targets(raw_targets)
 
-    for test in run_list:
+    for test in targets:
       test.run(self)
+
+  def report(self, raw_targets: list[str]):
+    targets: list[Test] = []
+    if len(raw_targets) == 0:
+      targets += (self.tests.get(TestKind.DIFF) or [])
+    else:
+      targets = self.get_targets(raw_targets)
+
+    for test in targets:
+      test.report(self)
 
 def main():
   argument_parser = argparse.ArgumentParser()
-  argument_parser.add_argument('action', type=str, nargs='*', help='Actions: clean, build, run')
+  argument_parser.add_argument('-a', '--action', type=str, nargs='*', help='Actions: clean, build, run')
+  argument_parser.add_argument('-t', '--target', type=str, nargs='*', help='Targets: `<kind>/<name>`')
   args = argument_parser.parse_args(sys.argv[1:])
 
-  do_clean = ('clean' in args.action)
-  do_build = ('build' in args.action)
-  do_run = ('run' in args.action)
+  actions = (args.action or [])
+  do_clean = ('clean' in actions)
+  do_build = ('build' in actions)
+  do_run = ('run' in actions)
+  do_report = ('report' in actions)
 
   framework = Framework.load_from_config('config.yml')
 
+  targets = (args.target or [])
   if do_clean:
-    framework.clean()
+    framework.clean(targets)
   if do_build:
-    framework.build()
+    framework.build(targets)
   if do_run:
-    framework.run()
+    framework.run(targets)
+  if do_report:
+    framework.report(targets)
 
 if __name__ == '__main__':
   main()
